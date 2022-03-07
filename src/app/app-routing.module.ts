637fa0f3ff0968e5845865b16fabdaf5f538624d
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-03-07 17:39:25 +0530

    nav bar and routeLink added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { HomeComponent } from './home/home.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"course",component:CourseComponent},
+  {path:"joinnow",component:JoinnowComponent},
+  {path:"about",component:AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CourseComponent,
+    JoinnowComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
